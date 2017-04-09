39703065f3a48862c2a2a85771a3139f5da364c3
AustinFelipe <austin.felipe@live.com>
2017-04-09 16:12:39 -0300

    Testando firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDI8Z4OjcqCieD7GJp31_gV8zG92S9iV7g",
+  authDomain: "hannah-2017e.firebaseapp.com",
+  databaseURL: "https://hannah-2017e.firebaseio.com",
+  projectId: "hannah-2017e",
+  storageBucket: "hannah-2017e.appspot.com",
+  messagingSenderId: "740973920753"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
